b6916b3729fe36c5291b9ff87b6d485c9f72a83e
wahyukmr <maryu9229@gmail.com>
2024-03-02 22:06:37 +0700

    [add] ➕ added the static banner

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
 import React from 'react';
 import {StatusBar, StyleSheet, Text, View, useColorScheme} from 'react-native';
+import {Banner, InputNewTask} from './components';
 
 export default function App() {
   const isDarkTheme = useColorScheme() === 'dark';
-  const backgroundColor = isDarkTheme ? '#000' : '#E8EAED';
+  const backgroundStyle = isDarkTheme ? '#1D1F21' : '#ffffff';
 
   return (
-    <View style={{...styles.viewContainer, backgroundColor}}>
+    <View style={{...styles.container, backgroundStyle}}>
       <StatusBar
         backgroundColor="transparent"
         translucent={true}
         barStyle={isDarkTheme ? 'light-content' : 'dark-content'}
       />
-      <View style={styles.viewContent}>
+      <View style={styles.contentWrapper}>
         <Text style={styles.textHeadline}>Today's tasks</Text>
+        <Banner />
+        <InputNewTask />
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  viewContainer: {
+  container: {
     flex: 1,
   },
-  viewContent: {
+  contentWrapper: {
+    fontSize: 16,
+    lineHeight: 24,
     paddingTop: 50,
     paddingHorizontal: 20,
   },
   textHeadline: {
     fontSize: 24,
+    lineHeight: 32,
     fontWeight: 'bold',
     marginTop: 30,
-    backgroundColor: 'yellow',
   },
 });
