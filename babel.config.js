f74d4e2f973b46b5103cdfcf649f3a296674bfef
wahyukmr <maryu9229@gmail.com>
2024-03-04 02:21:26 +0700

    [add] ➕ added and setup react-native-dotenv library

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,7 @@
-api.cache(false);
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [['module:react-native-dotenv']],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:@react-native/babel-preset'],
+    plugins: ['module:react-native-dotenv'],
+  };
 };
