ebfb7f4c566e1ccc9f207dd46495a07e10687e4d
wahyukmr <maryu9229@gmail.com>
2024-03-06 03:05:41 +0700

    [update] 🚀 refactoring file

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import Banner from './Banner';
-import Header from './Header';
-import InputNewTask from './InputNewTask';
-import TaskList from './TaskList';
-
-export {Banner, Header, InputNewTask, TaskList};
